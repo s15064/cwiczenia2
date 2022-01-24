d7ca3a93ce0a17f34ccffcc07162641b2a576fa3
wiewiorpl <wiewior.pl@gmail.com>
2022-01-24 12:59:33 +0100

    initial commit

---
+++ Person.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace csv2xml
+{
+    internal class Person
+    {
+        public string indexNumber;
+        public string fname;
+        public string lname;
+        public string birthdate;
+        public string email;
+        public string mothersName;
+        public string fathersName;
+        public string studies;
+        public string mode;
+
+        //Paweł,Nowak1,Informatyka dzienne,Dzienne,459,2000-02-12 00:00:00.000,nowak@pjwstk.edu.pl,Alina,Adam
+        // 0      1        2                  3     4             5                      6           7     8
+
+        public Person(string data)
+        {
+            string[] temp = data.Split(',');
+            indexNumber = temp[4];
+            fname = temp[0];
+            lname = temp[1];
+            birthdate = temp[5];
+            email = temp[6];
+            mothersName = temp[7];
+            fathersName = temp[8];
+            string tempS = temp[2].Split(' ')[0];
+            switch (tempS)
+            {
+                case "Informatyka":
+                    studies = "Computer Science";
+                    break;
+
+                case "Sztuka":
+                    studies = "New Media Art";
+                    break;
+                default:
+                    studies = "";
+                    break;
+
+            }
+            mode = temp[3];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace csv2xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string ifile = args[0];
+                string ofile = args[1];
+                string format = args[2];
+                string generationDate = DateTime.Now.ToString("dd.MM.yyyy");
+                string json = "{uczelnia:" +
+                              "{createdAt:\"" + generationDate + "\"," +
+                              "author:\"" + generationDate + "\"" +
+                              "studenci:[";
+
+                //Załadowanie danych z pliku
+                var students = new List<Person>();
+                var lines = System.IO.File.ReadAllLines(ifile);
+                foreach (string line in lines)
+                {
+                    Person tempPerson = new Person(line);
+                    students.Add(tempPerson);
+                }
+                Console.WriteLine("Data load finished.");
+                Console.WriteLine("Converting to ." + format);
+                foreach (Person p in students)
+                {
+                    json = json + "{" +
+                        "indexNumber:\"" + p.indexNumber + "\"," +
+                        "fname:\"" + p.fname + "\"," +
+                        "lname:\"" + p.lname + "\"," +
+                        "birthdate:\"" + p.birthdate + "\"," +
+                        "email:\"" + p.email + "\"," +
+                        "mothersName:\"" + p.mothersName + "\"," +
+                        "fathersName:\"" + p.fathersName + "\"," +
+                        "studies:{" +
+                            "name:\"" + p.studies + "\"," +
+                            "mode:\"" + p.mode + "\"},";
+
+                }
+                json = json.Remove(json.Length - 1, 1);
+
+                json = json + "],activeStudies:[{name:\"Computer Science\",numberOfStudents:\"";
+                int counter = 0;
+                foreach (Person p in students)
+                {
+                    if (p.studies.Equals("Computer Science"))
+                    { 
+                        counter++;
+                    }
+
+                }
+                json = json + counter + "\"},{name:\"New Media Art\",numberOfStudents:\"";
+                counter = 0;
+                foreach (Person p in students)
+                {
+                    if (p.studies.Equals("New Media Art"))
+                    {
+                        counter++;
+                    }
+
+                }
+                json = json + counter + "\"}]}}";
+            } 
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine("Podana ścieżka jest niepoprawna");
+            }
+            catch (FileNotFoundException fnf)
+            {
+                Console.WriteLine("Plik " + args[0] + "nie istnieje");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
